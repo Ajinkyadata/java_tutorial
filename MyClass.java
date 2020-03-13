f75830e3ecbc4d2f2bb35d5b0ed159d4f27919e2
Ajinkadata <ajinkyamesh8956@gmail.com>
2020-03-13 21:55:28 +0530

    modifier

---
+++ MyClass.java
@@ -0,0 +1,17 @@
+public class MyClass
+{
+    final int x=10;
+    final double p=3.14;
+    
+    
+    
+    
+    public static void main(String[] args)
+    {
+        MyClass obj=new MyClass();
+        obj.x=50;
+        obj.p=25;
+        
+        System.out.println(obj.x);
+    }
+}
