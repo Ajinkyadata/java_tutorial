d6c1d8174f2eb66350afa98552e1b45179112c23
Ajinkadata <ajinkyamesh8956@gmail.com>
2020-03-14 11:08:33 +0530

    constructors

---
+++ Car.java
@@ -0,0 +1,17 @@
+public class Car
+{
+    int year;
+    String name;
+
+    public Car(int yr, String nm)
+    {
+        year=yr;
+        name=nm;
+    }
+
+    public static void main(String[] args)
+    {
+        Car  newCar= new Car(2008, "hehe");
+        System.out.println(newCar.year+" "+newCar.name);
+    }
+}
