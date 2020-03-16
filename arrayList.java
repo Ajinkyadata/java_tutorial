d1283bff46818b342c3fb544294dea6484fc9bbf
Ajinkadata <ajinkyamesh8956@gmail.com>
2020-03-16 19:50:12 +0530

    Array

---
+++ arrayList.java
@@ -0,0 +1,19 @@
+ 
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class arrayList
+{
+    public static void main(String[] args)
+    {
+    ArrayList<Integer> obj= new ArrayList<Integer>();
+    
+    obj.add(2);
+    obj.add(3);
+    obj.add(7);
+    obj.add(8);
+    for(int i:obj)
+    System.out.println(i);
+    }
+    
+}
